6018ed19b1682336a608274e6b85622555a59c06
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-10-22 12:35:23 +0600

    added live site url in cors middleware

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://perfect-profile-resume.netlify.app",
+      "https://perfectprofile-ebde4.web.app",
     ],
     credentials: true,
   })
